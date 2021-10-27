25e51079827e05e4b68495f2ccb0c73e14234e97
drondel <realdrews@gmail.com>
2021-10-27 21:55:07 +0300

    Добавлена возможность вводить данные с клавиатуры

---
+++ HW.py
@@ -0,0 +1,26 @@
+import os, glob, re
+
+search_key = input('Введите нужное слово: ')
+trk = input('Введите путь до файла: ')
+
+'''if not trk.endswith('.log'):
+    os.chdir(trk)
+    for filename in glob.glob('*.log'):
+        f = open(filename, 'r')
+        for line in f.readlines():
+            if search_key in line:
+                print(line + ' ' + 'Найдено совпадение')
+                with open('parsing.log', 'a') as pars:
+                    pars.write(line)
+
+
+else:
+    os.chdir(os.path.join(trk, '..'))
+    f = open(trk, 'r')
+    print(os.getcwd())
+    for line in f.readlines():
+        if search_key in line:
+            print(line + ' ' + 'Найдено совпадение')
+            with open(os.path.join(os.getcwd(), 'justone.log'), 'a') as one:
+                one.write(line)
+'''
